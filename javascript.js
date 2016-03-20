566e8d01d0d97c8e150766c91f6ebb2b80607fc6
safwene baroudi <baroudi.safwene@gmail.com>
2016-03-20 19:10:24 +0100

    fixed some issues

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ context              = canvas.getContext('2d')
 context.font         = 'bold italic 97px Georgia'
 context.textBaseline = 'top'
 image                = new Image()
-image.src            = 'robin.gif'
+image.src            = 'baby.jpg'
 
 image.onload = function()
 {
